12942ffdd49edd56ab6aa4482ae14227409bcda4
Devi Firdaus <dfedogwa3@gmail.com>
2020-02-04 22:05:14 +0700

    add seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Create User To Create Organization and Account Manager
+user = User.new(email: "superadmin@mail.com", name: "Superadmin")
+user.password = "password123"
+user.save
+puts "Done Create User"
